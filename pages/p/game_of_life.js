e5f203cd86c789f29902e81479b8e44a9c1c42ac
tatiaris <tatiaris2001@gmail.com>
2021-01-09 03:10:07 -0600

    better seo for game of life page

--- pages/p/game_of_life.js
+++ pages/p/game_of_life.js
@@ -115,12 +115,12 @@ const Project = () => {
       <Head>
         <title>Rishabh Tatia | Game of Life</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Rishabh Tatia's personal website. Welcome to my portfolio page, here you can get to know me and my projects." />
+        <meta name="description" content="Play John Conway's Game of Life simulator created by Rishabh Tatia using React" />
         <meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=5, minimum-scale=1, width=device-width" />
         <meta property="og:title" content={`Rishabh Tatia | Game of Life`} />
         <meta property="og:site_name" content="Rishabh Tatia" />
         <meta property="og:url" content="tatiaris.com" />
-        <meta property="og:description" content="Rishabh Tatia's personal website. Welcome to my portfolio page, here you can get to know me and my projects." />
+        <meta property="og:description" content="Play John Conway's Game of Life simulator created by Rishabh Tatia using React" />
         <meta property="og:type" content="profile" />
         <meta httpEquiv="content-language" content="en" />
       </Head>
