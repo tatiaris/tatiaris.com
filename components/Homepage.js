6c62bdbc06368915bce407f30d0db3a42f077933
tatiaris <tatiaris2001@gmail.com>
2021-01-12 21:27:20 -0600

    small info update

--- components/Homepage.js
+++ components/Homepage.js
@@ -36,11 +36,12 @@ export const Homepage = (props) => {
         I started programming as a freshman in high school when I wanted to develop games, automate processes, and create helpful applications for the public.
       </Text>
         <ul>
-          <li>I entered the world on the 3rd of January 2001 in <a href="https://www.google.com/maps/place/Gwalior,+Madhya+Pradesh,+India/@28.7853139,59.1335284,3.83z/data=!4m5!3m4!1s0x3976c5d1792291fb:0xff4fb56d65bc3adf!8m2!3d26.2195191!4d78.1842041" className={`about-link city ${props.theme}`} target="_blank" rel="noreferrer">Gwalior, India</a>.</li>
+          <li>I was born on January 3rd 2001 in <a href="https://www.google.com/maps/place/Gwalior,+Madhya+Pradesh,+India/@28.7853139,59.1335284,3.83z/data=!4m5!3m4!1s0x3976c5d1792291fb:0xff4fb56d65bc3adf!8m2!3d26.2195191!4d78.1842041" className={`about-link city ${props.theme}`} target="_blank" rel="noreferrer">Gwalior, India</a>.</li>
           <li>I attended a boarding school for 4 years of my life, where I discovered my passion for sports and science.</li>
           <li>I moved to the U.S. in 2013, and attended high school in Chicago and Houston.</li>
           <li>I love playing sports and have played Table Tennis, Soccer, and Racquetball competitively.</li>
           <li>I enjoy participating in hackathons and am currently a developer and organizer for <a href="https://tamudatathon.com" className={`about-link datathon ${props.theme}`} target="_blank" rel="noreferrer">TAMU Datathon</a>.</li>
+          <li>I am currently a web developer at <a href="https://acelab.tamu.edu" className={`about-link tamu ${props.theme}`} target="_blank" rel="noreferrer">Acelab</a> and will be interning at <a href="https://intuit.com" className={`about-link intuit ${props.theme}`} target="_blank" rel="noreferrer">Intuit</a> over Summer 2021.</li>
           <li>I like listening to Alternative, Pop, EDM, Hip-hop and Rock music.</li>
           <li>I aim to spread the knowledge of programming and computer science as much as poossible.</li>
           <li>My life long goal is to travel and document as many places around the world as I can.</li>
