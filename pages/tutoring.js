999c6153e4d7e9f739b2ccec32afa3bfe47ad259
tatiaris <tatiaris2001@gmail.com>
2021-01-18 02:46:29 -0600

    updated info tutoring page

--- pages/tutoring.js
+++ pages/tutoring.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
-import { Page, Breadcrumbs, Text, Image } from '@geist-ui/react';
+import { Page, Breadcrumbs, Text, Image, Divider } from '@geist-ui/react';
 
 const Tutoring = () => {
   return (
     <Page id="project-container">
       <Head>
-        <title>Rishabh Tatia | CS Tutoring</title>
+        <title>Rishabh Tatia | Computer Science Tutoring</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Computer Science tutoring services offered by Rishabh Tatia." />
         <meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=5, minimum-scale=1, width=device-width" />
-        <meta property="og:title" content={`Rishabh Tatia | CS Tutoring`} />
+        <meta property="og:title" content={`Rishabh Tatia | Computer Science Tutoring`} />
         <meta property="og:site_name" content="Rishabh Tatia Tutoring" />
         <meta property="og:url" content="tatiaris.com/tutoring" />
         <meta property="og:description" content="Computer Science tutoring services offered by Rishabh Tatia." />
@@ -22,13 +22,13 @@ const Tutoring = () => {
         <Breadcrumbs.Item>tutoring</Breadcrumbs.Item>
       </Breadcrumbs>
       <br />
-      <Text h3>COMPUTER SCIENCE TUTORING</Text>
+      <Text h3>RISHABH&apos;S COMPUTER SCIENCE TUTORING</Text>
       <br />
       <br />
       <Image width={600} height={300} src="/tutoring/programmer_1.svg" alt="programmer_1" />
       <br />
       <br />
-      <Text h4>WHAT?</Text>
+      <Text h4>WHAT is Computer Science?</Text>
       <Text style={{ marginTop: 0 }}>
         <b>Computer science</b> is the study of computers and computing as well as their <b>theoretical</b> and <b>practical</b> applications. The theory of computer science includes topics such as
         data structures, algorithms, computer and network design, data science, and more. The applications of computer science include machine learning, software (web/app/game/system) development,
@@ -38,17 +38,17 @@ const Tutoring = () => {
         </a>{' '}
         .
       </Text>
-      <Text h4>WHY?</Text>
+      <Text h4>WHY should you learn it?</Text>
       <Text style={{ marginTop: 0 }}>
-        Good computer scientists are in <b>high demand</b> today, and are expected to stay so for a very long time. Even if one does not choose to work a computer science job, programming is an{' '}
-        <b>extremely beneficial skill</b> which can be applied to <b>almost any field</b> and one&apos;s personal life as well. If neither of the above statements interest you, you can still{' '}
-        <b>have fun</b> solving challenging and interesting{' '}
+        Good computer scientists are in <b>high demand</b> today, and are expected to stay so for a very long time. Even if you do not choose to work a computer science job, programming is an{' '}
+        <b>extremely beneficial skill</b> which can be applied to <b>almost any field</b> and your personal life as well. If neither of the above statements interest you, you can still <b>have fun</b>{' '}
+        solving challenging and interesting{' '}
         <a href="https://open.kattis.com/" className="color-link red-bg" target="_blank" rel="noreferrer">
           logic puzzles
         </a>{' '}
         through programming.
       </Text>
-      <Text h4>WHO?</Text>
+      <Text h4>WHO can help you learn?</Text>
       <Text style={{ marginTop: 0 }}>
         Me! My name is{' '}
         <a href="https://tatiaris.com/" className="color-link green-bg" target="_blank" rel="noreferrer">
@@ -58,7 +58,31 @@ const Tutoring = () => {
         <a href="https://tamu.edu/" className="color-link maroon-bg" target="_blank" rel="noreferrer">
           Texas A&M University
         </a>
-        .
+        . I have <b>6 years of experience</b> programming and developing various softwares and am <b>passionate about teaching</b> this useful skill to anyone willing to learn.
+      </Text>
+      <Text h4>HOW will you learn?</Text>
+      <Text style={{ marginTop: 0 }}>
+        We will be using{' '}
+        <a href="https://zoom.us/download/" className="color-link zoom-bg" target="_blank" rel="noreferrer">
+          Zoom
+        </a>{' '}
+        to conduct <b>one-on-one</b> classes at predetermined times of the week based on availability.
+      </Text>
+      <Divider y={5}>CLASSES</Divider>
+      <Text h4>
+        Intro to Programming | <span style={{ color: 'hotpink' }}>$30/Hr</span>
+      </Text>
+      <Text h4>
+        AP Computer Science Prep | <span style={{ color: 'hotpink' }}>$30/Hr</span>
+      </Text>
+      <Text h4>
+        Web Development | <span style={{ color: 'hotpink' }}>$40/Hr</span>
+      </Text>
+      <Text h4>
+        Problem Solving | <span style={{ color: 'hotpink' }}>$40/Hr</span>
+      </Text>
+      <Text small style={{ float: 'right' }} type="secondary" className={`copyright-statement light`}>
+        Copyright © 2020 - 2021 Rishabh Tatia
       </Text>
     </Page>
   );
