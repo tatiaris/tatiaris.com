784a237bc5c03e325d4192d8079028379c0cccff
tatiaris <tatiaris2001@gmail.com>
2021-01-19 02:46:29 -0600

    somewhat finalized tutoring page

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,90 +1,155 @@
-import React from "react";
-import { Text, User, Divider, Row, Tag, Spacer, Col, Badge, Link, useToasts, Tooltip } from '@geist-ui/react'
-import { Github, FileText, Instagram, Twitter, Linkedin, Mail } from '@geist-ui/react-icons'
+import React from 'react';
+import { Text, User, Divider, Row, Tag, Spacer, Col, Badge, Link, useToasts, Tooltip } from '@geist-ui/react';
+import { Github, FileText, Instagram, Twitter, Linkedin, Mail } from '@geist-ui/react-icons';
 
 /**
  * Homepage component
  */
 export const Homepage = (props) => {
-  const [toasts, setToast] = useToasts()
-  const launchToast = (msg) => setToast({ text: msg, type: "success", delay: 3000 })
+  const [toasts, setToast] = useToasts();
+  const launchToast = (msg) => setToast({ text: msg, type: 'success', delay: 3000 });
 
   return (
     <>
       <div className="content-wrapper">
-        <Row style={{ alignItems: "center", flexWrap: "wrap", marginTop: "1em" }}>
-          <Col style={{ width: "max-content", marginTop: "1em", marginRight: "1em" }}>
-            <User src="img/avatar.png" altText="user-img" name="Rishabh Tatia" className={`user-tag-${props.theme}`} style={{ padding: "0" }}>
+        <Row style={{ alignItems: 'center', flexWrap: 'wrap', marginTop: '1em' }}>
+          <Col style={{ width: 'max-content', marginTop: '1em', marginRight: '1em' }}>
+            <User src="img/avatar.png" altText="user-img" name="Rishabh Tatia" className={`user-tag-${props.theme}`} style={{ padding: '0' }}>
               Full Stack Developer
-          </User>
+            </User>
           </Col>
-          <Col style={{ width: "max-content", marginTop: "1em" }}>
+          <Col style={{ width: 'max-content', marginTop: '1em' }}>
             <Row>
-              <Badge type="secondary" className={`badge-link ${props.theme}`}><a href="https://intuit.com" target="_blank" rel="noreferrer">@INTUIT</a></Badge>
-              <Spacer x={.5} />
-              <Badge type="secondary" className={`badge-link ${props.theme}`}><a href="https://acelab.tamu.edu" target="_blank" rel="noreferrer">@ACELAB</a></Badge>
-              <Spacer x={.5} />
-              <Badge type="secondary" className={`badge-link ${props.theme}`}><a href="https://tamudatathon.com" target="_blank" rel="noreferrer">@TAMU_DATATHON</a></Badge>
+              <Badge type="secondary" className={`badge-link ${props.theme}`}>
+                <a href="https://intuit.com" target="_blank" rel="noreferrer">
+                  @INTUIT
+                </a>
+              </Badge>
+              <Spacer x={0.5} />
+              <Badge type="secondary" className={`badge-link ${props.theme}`}>
+                <a href="https://acelab.tamu.edu" target="_blank" rel="noreferrer">
+                  @ACELAB
+                </a>
+              </Badge>
+              <Spacer x={0.5} />
+              <Badge type="secondary" className={`badge-link ${props.theme}`}>
+                <a href="https://tamudatathon.com" target="_blank" rel="noreferrer">
+                  @TAMU_DATATHON
+                </a>
+              </Badge>
             </Row>
           </Col>
         </Row>
         <br />
         <Divider align="start">My Life</Divider>
         <Text>
-          <span style={{ fontSize: "1.2em" }}>Hello,</span><br />
-        My name is <b>Rishabh Tatia</b>. I am a Junior at <a href="https://tamu.edu" className={`about-link tamu ${props.theme}`} target="_blank" rel="noreferrer">Texas A&M University</a> studying Mathematics and Computer Science.
-        I started programming as a freshman in high school when I wanted to develop games, automate processes, and create helpful applications for the public.
-      </Text>
+          <span style={{ fontSize: '1.2em' }}>Hello,</span>
+          <br />
+          My name is <b>Rishabh Tatia</b>. I am a Junior at{' '}
+          <a href="https://tamu.edu" className={`about-link tamu ${props.theme}`} target="_blank" rel="noreferrer">
+            Texas A&M University
+          </a>{' '}
+          studying Mathematics and Computer Science. I started programming as a freshman in high school when I wanted to develop games, automate processes, and create helpful applications for the
+          public.
+        </Text>
         <ul>
-          <li>I was born on January 3rd 2001 in <a href="https://www.google.com/maps/place/Gwalior,+Madhya+Pradesh,+India/@28.7853139,59.1335284,3.83z/data=!4m5!3m4!1s0x3976c5d1792291fb:0xff4fb56d65bc3adf!8m2!3d26.2195191!4d78.1842041" className={`about-link city ${props.theme}`} target="_blank" rel="noreferrer">Gwalior, India</a>.</li>
+          <li>
+            I was born on January 3rd 2001 in{' '}
+            <a
+              href="https://www.google.com/maps/place/Gwalior,+Madhya+Pradesh,+India/@28.7853139,59.1335284,3.83z/data=!4m5!3m4!1s0x3976c5d1792291fb:0xff4fb56d65bc3adf!8m2!3d26.2195191!4d78.1842041"
+              className={`about-link city ${props.theme}`}
+              target="_blank"
+              rel="noreferrer">
+              Gwalior, India
+            </a>
+            .
+          </li>
           <li>I attended a boarding school for 4 years of my life, where I discovered my passion for sports and science.</li>
           <li>I moved to the U.S. in 2013, and attended high school in Chicago and Houston.</li>
           <li>I love playing sports and have played Table Tennis, Soccer, and Racquetball competitively.</li>
-          <li>I enjoy participating in hackathons and am currently a developer and organizer for <a href="https://tamudatathon.com" className={`about-link datathon ${props.theme}`} target="_blank" rel="noreferrer">TAMU Datathon</a>.</li>
-          <li>I am currently a web developer at <a href="https://acelab.tamu.edu" className={`about-link tamu ${props.theme}`} target="_blank" rel="noreferrer">Acelab</a> and will be interning at <a href="https://intuit.com" className={`about-link intuit ${props.theme}`} target="_blank" rel="noreferrer">Intuit</a> over Summer 2021.</li>
+          <li>
+            I enjoy participating in hackathons and am currently a developer and organizer for{' '}
+            <a href="https://tamudatathon.com" className={`about-link datathon ${props.theme}`} target="_blank" rel="noreferrer">
+              TAMU Datathon
+            </a>
+            .
+          </li>
+          <li>
+            I am currently a web developer at{' '}
+            <a href="https://acelab.tamu.edu" className={`about-link tamu ${props.theme}`} target="_blank" rel="noreferrer">
+              Acelab
+            </a>{' '}
+            and will be interning at{' '}
+            <a href="https://intuit.com" className={`about-link intuit ${props.theme}`} target="_blank" rel="noreferrer">
+              Intuit
+            </a>{' '}
+            over Summer 2021.
+          </li>
           <li>I like listening to Alternative, Pop, EDM, Hip-hop and Rock music.</li>
-          <li>I aim to spread the knowledge of programming and computer science as much as poossible.</li>
+          <li>
+            I am passionate about teaching and{' '}
+            <a href="/tutoring" className={`about-link tutoring ${props.theme}`}>
+              privately tutor
+            </a>{' '}
+            people in computer science and it&apos;s applications.
+          </li>
           <li>My life long goal is to travel and document as many places around the world as I can.</li>
         </ul>
-        <Divider style={{ marginTop: "2.5em" }} align="start">My Hobbies & Interests</Divider>
-        <Row style={{ flexWrap: "wrap" }}>
+        <Divider style={{ marginTop: '2.5em' }} align="start">
+          My Hobbies & Interests
+        </Divider>
+        <Row style={{ flexWrap: 'wrap' }}>
           <Tag className={`hobby ${props.theme}`}>Racquetball</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Longboarding</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Ping Pong</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Basketball</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Pool</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Video Games</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Board Games</Tag>
-          <Spacer x={.5} />
+          <Spacer x={0.5} />
           <Tag className={`hobby ${props.theme}`}>Speed Cubing</Tag>
         </Row>
-        <Divider style={{ marginTop: "2.5em" }} align="start">My Links</Divider>
-        <Row style={{ flexWrap: "wrap" }} className={`${props.theme}`}>
+        <Divider style={{ marginTop: '2.5em' }} align="start">
+          My Links
+        </Divider>
+        <Row style={{ flexWrap: 'wrap' }} className={`${props.theme}`}>
           <Tooltip text={'Github'} placement="bottom" enterDelay="1000">
-            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://github.com/tatiaris" block><Github /></Link>
+            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://github.com/tatiaris" block>
+              <Github />
+            </Link>
           </Tooltip>
           <Tooltip text={'Linkedin'} placement="bottom" enterDelay="1000">
-            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tatiaris/" block><Linkedin /></Link>
+            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tatiaris/" block>
+              <Linkedin />
+            </Link>
           </Tooltip>
           <Tooltip text={'Instagram'} placement="bottom" enterDelay="1000">
-            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://www.instagram.com/tat.ris/" block><Instagram /></Link>
+            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://www.instagram.com/tat.ris/" block>
+              <Instagram />
+            </Link>
           </Tooltip>
           <Tooltip text={'Twitter'} placement="bottom" enterDelay="1000">
-            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://twitter.com/Rishabh77022392" block><Twitter /></Link>
+            <Link className="misc-link" target="_blank" rel="noreferrer" href="https://twitter.com/Rishabh77022392" block>
+              <Twitter />
+            </Link>
           </Tooltip>
           <Tooltip text={'E-mail'} placement="bottom" enterDelay="1000">
-            <Link className="misc-link" target="_blank" rel="noreferrer" href="mailto:tatiaris2001@gmail.com?Subject=Hello!" block><Mail /></Link>
+            <Link className="misc-link" target="_blank" rel="noreferrer" href="mailto:tatiaris2001@gmail.com?Subject=Hello!" block>
+              <Mail />
+            </Link>
           </Tooltip>
-          <Link download onClick={e => launchToast('Downloading Resume PDF...')} className="misc-link" href="/docs/RISHABH_TATIA_RESUME_12_20_v1.pdf" block><FileText /><span style={{ marginLeft: "5px" }}>Resume</span></Link>
+          <Link download onClick={(e) => launchToast('Downloading Resume PDF...')} className="misc-link" href="/docs/RISHABH_TATIA_RESUME_12_20_v1.pdf" block>
+            <FileText />
+            <span style={{ marginLeft: '5px' }}>Resume</span>
+          </Link>
         </Row>
       </div>
-
     </>
   );
 };
--- pages/tutoring.js
+++ pages/tutoring.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import { Page, Breadcrumbs, Text, Image, Divider, Row, Col } from '@geist-ui/react';
+import { Page, Breadcrumbs, Text, Image, Divider, Row, Tooltip, Link, useToasts } from '@geist-ui/react';
+import { Github, FileText, Linkedin, Mail, Globe } from '@geist-ui/react-icons';
 
 const Tutoring = () => {
+  const [toasts, setToast] = useToasts();
+  const launchToast = (msg) => setToast({ text: msg, type: 'success', delay: 3000 });
   return (
-    <Page id="project-container">
+    <Page id="tutoring-page-container">
       <Head>
         <title>Rishabh Tatia | Computer Science Tutoring</title>
         <link rel="icon" href="/favicon.ico" />
@@ -25,7 +28,7 @@ const Tutoring = () => {
       <Text h3>RISHABH&apos;S COMPUTER SCIENCE TUTORING</Text>
       <br />
       <br />
-      <Image width={600} height={300} src="/tutoring/programmer_1.svg" alt="programmer_1" />
+      <Image width={700} height={350} src="/tutoring/programmer_1.svg" alt="programmer_1" />
       <br />
       <br />
       <Text h4>WHAT is Computer Science?</Text>
@@ -68,23 +71,25 @@ const Tutoring = () => {
         </a>{' '}
         to conduct <b>one-on-one</b> classes at predetermined times of the week based on availability.
       </Text>
-      <Divider y={5}>CLASSES</Divider>
+      <Divider align="start" y={5}>
+        CLASSES
+      </Divider>
       <Row className="class-info-container" style={{ flexWrap: 'wrap', justifyContent: 'center' }}>
         <div className="class-image-container">
-          <Image width={400} height={200} src="/tutoring/intro_programmer.svg" alt="intro_programmer" />
+          <Image src="/tutoring/intro_programmer.svg" alt="intro_programmer" />
         </div>
         <div className="class-details">
           <Text h4>
             Intro to Programming | <span style={{ color: 'hotpink' }}>$30/Hr</span>
           </Text>
           <Text>
-            Learn what programming is, how it is used in our world today, and the basics of any{' '}
+            Learn about programming, how it is used today, and the basics of any{' '}
             <a href="https://www.geeksforgeeks.org/difference-between-high-level-and-low-level-languages/" className="color-link green-bg" target="_blank" rel="noreferrer">
               high-level programming language
             </a>{' '}
             of your choice.
           </Text>
-          <span className="goals">Goals:</span>
+          <span className="goals">Objectives:</span>
           <ul>
             <li>
               Understand <b>fundamentals of programming</b> such as variables, conditions, loops, and methods.
@@ -99,7 +104,7 @@ const Tutoring = () => {
       <Divider />
       <Row className="class-info-container" style={{ flexWrap: 'wrap', justifyContent: 'center' }}>
         <div className="class-image-container order-one">
-          <Image width={400} height={200} src="/tutoring/ap_cs_prep.svg" alt="ap_cs_prep" />
+          <Image src="/tutoring/ap_cs_prep.svg" alt="ap_cs_prep" />
         </div>
         <div className="class-details">
           <Text h4>
@@ -108,17 +113,17 @@ const Tutoring = () => {
           <Text>
             Master the content that appears on the <b>AP Computer Science A</b> exam and get a 5 on it!
           </Text>
-          <span className="goals">Goals:</span>
+          <span className="goals">Objectives:</span>
           <ul>
             <li>
-              Learn how{' '}
+              Understand how{' '}
               <a href="https://en.wikipedia.org/wiki/Java_(programming_language)" className="color-link orange-bg" target="_blank" rel="noreferrer">
                 Java
               </a>{' '}
               works along with the concept and principles of <b>object oriented programming</b>.
             </li>
             <li>
-              Learn how to <b>debug</b>, fix errors, document code, and <b>explain</b> how a program works.
+              Understand how to <b>debug</b>, fix errors, document code, and <b>explain</b> how a program works.
             </li>
             <li>
               Have the ability to design programs, <b>develop algorithms</b>, and implement them.
@@ -129,28 +134,81 @@ const Tutoring = () => {
       <Divider />
       <Row className="class-info-container" style={{ flexWrap: 'wrap', justifyContent: 'center' }}>
         <div className="class-image-container">
-          <Image width={400} height={200} src="/tutoring/web_dev_1.svg" alt="intro_programmer" />
+          <Image src="/tutoring/web_dev_1.svg" alt="intro_programmer" />
         </div>
-        <div style={{ width: '550px' }}>
+        <div className="class-details">
           <Text h4>
             Web Development | <span style={{ color: 'hotpink' }}>$40/Hr</span>
           </Text>
-          <Text>Learn how to design, build, and publish beautiful, responsive websites using HTML, CSS, and Javascript.</Text>
+          <Text>
+            Design, build, and publish beautiful, <b>responsive websites</b> using HTML, CSS, and Javascript.
+          </Text>
+          <span className="goals">Objectives:</span>
+          <ul>
+            <li>
+              Understand how to use HTML, CSS, and Javascript to develop <b>static and dynamic webpages</b>.
+            </li>
+            <li>
+              Know about the different <b>libraries and frameworks</b> popular across the internet.
+            </li>
+            <li>
+              Have the ability to use the acquired knowledge to develop a <b>web-app from scratch</b>.
+            </li>
+          </ul>
         </div>
       </Row>
       <Divider />
       <Row className="class-info-container" style={{ flexWrap: 'wrap', justifyContent: 'center' }}>
         <div className="class-image-container order-one">
-          <Image width={400} height={200} src="/tutoring/problem_solving_2.svg" alt="problem_solving_2" />
+          <Image src="/tutoring/problem_solving_2.svg" alt="problem_solving_2" />
         </div>
-        <div style={{ width: '550px' }}>
+        <div className="class-details">
           <Text h4>
             Problem Solving | <span style={{ color: 'hotpink' }}>$40/Hr</span>
           </Text>
-          <Text></Text>
+          <Text>
+            Use advanced <b>programming logic</b> to solve complex and challenging coding problems.
+          </Text>
+          <span className="goals">Objectives:</span>
+          <ul>
+            <li>
+              Learn what different <b>data structures and algorithms</b> are and when to use them.
+            </li>
+            <li>
+              Learn about <b>Big O Notation</b> and use it to analyze algorithms and their time complexity.
+            </li>
+            <li>
+              Develop <b>critical thinking</b> skills to understand, analyze, and solve coding problems programatically.
+            </li>
+          </ul>
         </div>
       </Row>
-      <Divider y={5} />
+      <Divider align="start" y={5}>
+        GET IN TOUCH
+      </Divider>
+      <Row style={{ flexWrap: 'wrap' }} className="ligt">
+        <Link className="misc-link" target="_blank" rel="noreferrer" href="mailto:tatiaris2001@gmail.com?Subject=Hello!" block>
+          <Mail />
+          <span style={{ marginLeft: '5px' }}>tatiaris2001@gmail.com</span>
+        </Link>
+        <Link className="misc-link" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tatiaris/" block>
+          <Linkedin />
+          <span style={{ marginLeft: '5px' }}>Linkedin</span>
+        </Link>
+        <Link className="misc-link" target="_blank" rel="noreferrer" href="https://github.com/tatiaris" block>
+          <Github />
+          <span style={{ marginLeft: '5px' }}>Github</span>
+        </Link>
+        <Link className="misc-link" href="/" block>
+          <Globe />
+          <span style={{ marginLeft: '5px' }}>Portfolio</span>
+        </Link>
+        <Link download onClick={(e) => launchToast('Downloading Resume PDF...')} className="misc-link" href="/docs/RISHABH_TATIA_RESUME_12_20_v1.pdf" block>
+          <FileText />
+          <span style={{ marginLeft: '5px' }}>Resume</span>
+        </Link>
+      </Row>
+      <Divider y={5}></Divider>
       <Text small style={{ float: 'right' }} type="secondary" className={`copyright-statement light`}>
         Copyright © 2020 - 2021 Rishabh Tatia
       </Text>
