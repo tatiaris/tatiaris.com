a6dbe5030d8b2436142254335764c3cb066d09b6
tatiaris <tatiaris2001@gmail.com>
2021-01-31 12:37:26 -0600

    added test data

--- pages/inapp.js
+++ pages/inapp.js
@@ -22,9 +22,18 @@ const Inapp = () => {
       <Spacer y={2} />
       <Text h4>Text Examples</Text>
       <Spacer y={1} />
-      <Description title="Appropriate Text" content="This section contains only appropriate and safe for work text data." />
+      <Description
+        title="Appropriate Text"
+        content="The short began with Chloe Fineman giving us her best Nicole Kidman, as she crooned The Undoing‘s opening tune, “Dream a Little Dream of Me.” (“Did you notice my coat?” she asks.) From there we were served new renditions of the songs from Bridgerton (by way of Lady Whistledown aka Julie Andrews), The Mandalorian, The Queen’s Gambit, Succession and more, as Cecily Strong, Kate McKinnon, Beck Bennett, Melissa Villaseñor, Pete Davidson, Kyle Mooney, Alex Moffat and all stepped in to exercise their vocal talents."
+      />
       <Spacer y={1} />
-      <Description title="Inappropriate Text" content="This section fuckin only contains shit that is never fkin safe for work bitch." />
+      <Description
+        title="Inappropriate Text"
+        content="Why are men like diapers? They’re usually full of shit, but thankfully disposable.
+What did one butt cheek say to the other? Together, we can stop this shit. 
+What did the hurricane say to the coconut tree? Hold on to your nuts, this ain’t no ordinary blowjob. 
+Which sexual position produces the ugliest kids? Ask your mum!"
+      />
       <Spacer y={2} />
       <Text h4>Image Examples</Text>
       <Spacer y={1} />
