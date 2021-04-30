2b5a56b107e9919fed218a76c5059be4c06e5a54
tatiaris <tatiaris2001@gmail.com>
2021-04-29 19:39:22 -0500

    thumbnail meta data

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,9 @@ export default function Home() {
         <Head>
           <title>Rishabh Tatia | Portfolio</title>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Rishabh Tatia's personal website. Welcome to my portfolio page, here you can get to know me and my projects." />
           <meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=5, minimum-scale=1, width=device-width" />
+          <meta name="description" content="Rishabh Tatia's personal website. Welcome to my portfolio page, here you can get to know me and my projects." />
+          <meta name="thumbnail" content="https://tatiaris.com/img/thumbnail.png" />
           <meta property="og:title" content={`Rishabh Tatia | Portfolio`} />
           <meta property="og:site_name" content="Rishabh Tatia" />
           <meta property="og:url" content="tatiaris.com" />
