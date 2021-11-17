733c9af4eca526a3a4674044848f86e6c107481a
Rishabh <tatiaris2001@gmail.com>
2021-11-17 05:56:06 -0600

    golay update

--- components/slides/396/golay-code.jsx
+++ components/slides/396/golay-code.jsx
@@ -9,8 +9,16 @@ export const GolayCode = () => {
       <div className="title-container">
         <h1>Binary Golay Code</h1>
       </div>
-      <div className="slide-content-container">
-        <img width="700" src="/img/slides/golay-code.png" alt="" style={{ marginLeft: '350px' }} />
+      <div className="slide-content-container" style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>
+          <ul>
+            <li>k = 12 (length of input)</li>
+            <li>n = 23 (length of encoded bitstring)</li>
+            <li>Can correct 3-bit errors</li>
+            <li>Can detect up to 7-bit errors</li>
+          </ul>
+        </div>
+        <img width="600" src="/img/slides/golay-code.png" alt="" />
       </div>
     </div>
   );
